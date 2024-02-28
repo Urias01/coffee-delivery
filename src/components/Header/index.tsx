81cec43b801dc8af3bd0b2a581c42ca3f09a6ee8
Urias Pereira <uriasluis64@gmail.com>
2024-02-28 11:16:56 -0300

    header coffee delivery

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
-import { HeaderContainer } from './styles'
+import {
+  HeaderContainer,
+  LocationButton,
+  NavContainer,
+  ShoppingCartButton,
+} from './styles'
 import LogoCoffee from '../../assets/logo-coffee.svg'
+import { MapPin, ShoppingCart } from 'phosphor-react'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={LogoCoffee} alt="" />
-      <nav></nav>
+      <NavContainer>
+        <LocationButton>
+          <MapPin size={24} color="#4b2995" weight="fill" />
+          São Paulo, SP
+        </LocationButton>
+
+        <ShoppingCartButton>
+          <ShoppingCart size={24} color="#c47f17" weight="fill" />
+        </ShoppingCartButton>
+      </NavContainer>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10rem;
+  height: 100px;
+  img {
+    width: 85px;
+    height: 40px;
+  }
+`
+
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const LocationButton = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme['purple-light']};
+  color: ${(props) => props.theme['purple-dark']};
+
+  cursor: pointer;
+`
+
+export const ShoppingCartButton = styled.span`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme['yellow-light']};
+
+  cursor: pointer;
+`
